eb583556c0185566336ba718a6faba11c10e3a55
RogerOliveira <rogerolivierhp@gmail.com>
2018-06-02 18:20:53 -0300

    abstract factory

---
+++ arqsw_criacionais_a7/src/abstract_factroty_exercicio1_1/AbstractHelloWorld.java
@@ -0,0 +1,7 @@
+package abstract_factroty_exercicio1_1;
+
+public interface AbstractHelloWorld {
+	
+	void printHelloWord();
+
+}
---
+++ arqsw_criacionais_a7/src/abstract_factroty_exercicio1_1/PrintHelloWorld.java
@@ -0,0 +1,10 @@
+package abstract_factroty_exercicio1_1;
+
+public class PrintHelloWorld implements AbstractHelloWorld {
+
+	@Override
+	public void printHelloWord() {
+		System.out.println("Hello World");
+	}
+
+}
